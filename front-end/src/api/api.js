ea6a11ef7b5a96f305469b8aa440b12ba0c191a9
Tales Rodrigues <tales00001@gmail.com>
2023-01-24 18:39:58 -0300

    Corrigindo Requisitos 4 e 5

--- front-end/src/api/api.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios'
-
-const instance = axios.create({
-    baseURL: 'http://localhost:3001'
-})
-
-export const loginUser = async({email, password}) => {
-    return instance.post('login', { email, password })
-}
