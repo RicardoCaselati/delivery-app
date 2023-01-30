c43411b18036d345b7cd7fb2f60df3900252cbb0
Ricardo Gabriel <riicardogabriel12@gmail.com>
2023-01-30 19:05:35 -0300

    Feito altereções da resposta underscored

--- back-end/src/controllers/saleController.js
+++ back-end/src/controllers/saleController.js
@@ -1,14 +1,21 @@
 const saleService = require('../services/sale.service');
 
+const userId = 'user_id';
+const sellerId = 'seller_id';
+const totalPrice = 'total_price';
+const deliveryAddress = 'delivery_address';
+const deliveryNumber = 'delivery_number';
+const saleDate = 'sale_date';
+
 const createSale = async (req, res) => {
     const sale = {
-      userId: req.body.userId,
-      sellerId: req.body.sellerId,
-      totalPrice: req.body.totalPrice,
-      deliveryAddress: req.body.deliveryAddress,
-      deliveryNumber: req.body.deliveryNumber,
+      [userId]: req.body.userId,
+      [sellerId]: req.body.sellerId,
+      [totalPrice]: req.body.totalPrice,
+      [deliveryAddress]: req.body.deliveryAddress,
+      [deliveryNumber]: req.body.deliveryNumber,
       cart: req.body.cart,
-      saleDate: new Date(),
+      [saleDate]: new Date(),
     };
 
     const saleCreated = await saleService.createSale(sale);
